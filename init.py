c46c2f683cd5ed634330d29afb92ad7722443730
jallal-msd <jallalblack@gmail.com>
2025-05-10 18:12:02 +0200

    init

--- init.py
+++ init.py
@@ -1,3 +1,9 @@
 import os
 def init(repo):
     os.mkdir(repo)
+    os.mkdir(os.path.join(repo, '.git'))
+    for name in ['objects', 'refs', 'refs/head']:
+        os.mkdir(os.path.join(repo, '.git', name))
+    # write_file(os.path.join(repo, '.git', 'HEAD'),b'ref: refs/heads/master')
+    print('Initilaized empty repository: {}'. format(repo))
+
